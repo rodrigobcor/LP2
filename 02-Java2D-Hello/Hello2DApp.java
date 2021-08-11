c79e1f83395a8c40249cda734a015476becc1a29
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-08-11 15:20:56 -0300

    Update Hello2DApp.java

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.geom.*;
 
 public class Hello2DApp {
     public static void main (String[] args) {
@@ -18,17 +19,35 @@ class Hello2DFrame extends JFrame {
             }
         );
         this.setTitle("Java2D - Hello World!");
-        this.setSize(350, 350);
+        this.setSize(400, 400);
+        this.getContentPane().setBackground(Color.BLACK);
         this.setVisible(true);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.blue);
+        Color verdeoliva = new Color (110,139,61);
+        g2d.setColor(verdeoliva);
         int w = getWidth();
         int h = getHeight();
-        g2d.drawLine(0,0, w,h);
-        g2d.drawLine(0,h, w,0);
+        g2d.drawString("Hello World!", 167, 217);
+        CubicCurve2D a = new CubicCurve2D.Double();
+        a.setCurve(50, 74, 150,
+            44, 250, 104, 350, 74);
+        g2d.draw(a);
+        CubicCurve2D b = new CubicCurve2D.Double();
+        b.setCurve(50, 74, 80,
+            158, 30, 242, 50, 350);
+        g2d.draw(b);
+        CubicCurve2D c = new CubicCurve2D.Double();
+        c.setCurve(50, 350, 150,
+            320, 250, 370, 350, 350);
+        g2d.draw(c);
+        CubicCurve2D d = new CubicCurve2D.Double();
+        d.setCurve(350, 74, 380,
+            158, 320, 242, 350, 350);
+        g2d.draw(d);
+        g2d.draw(new Rectangle2D.Double(160, 203, 80, 20));
     }
 }
