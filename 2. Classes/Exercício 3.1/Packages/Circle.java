c97dfe3f5e9889bf84d1b77deddbb4565ec4aed8
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-08-26 15:42:48 -0300

    Create Circle.java

---
+++ 2. Classes/Exercício 3.1/Packages/Circle.java
@@ -0,0 +1,25 @@
+package figures;
+
+import java.awt.*;
+
+public class Circle {
+    int x, y;
+    int w, h;
+
+    public Circle (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public void print () {
+        System.out.format("Circulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawOval(this.x,this.y, this.w,this.h);
+    }
+}
