d7863ec727a9aba85346a6657770a5097a92c127
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-08-26 15:43:05 -0300

    Create Arc.java

---
+++ 2. Classes/Exercício 3.1/Packages/Arc.java
@@ -0,0 +1,28 @@
+package figures;
+
+import java.awt.*;
+
+public class Arc {
+    int x, y;
+    int w, h;
+    int a1, a2;
+
+    public Arc (int x, int y, int w, int h, int a1, int a2) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.a1 = a1;
+        this.a2 = a2;
+    }
+
+    public void print () {
+        System.out.format("Circulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawArc(this.x,this.y, this.w,this.h, this.a1,this.a2);
+    }
+}
