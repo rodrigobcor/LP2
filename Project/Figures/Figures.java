87822984d1ff827372249d05f711709287c634e7
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-09-19 18:43:53 -0300

    Update Figures.java

--- Project/Figures/Figures.java
+++ Project/Figures/Figures.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 public abstract class Figure {
     public int x, y;
     public int w, h;
-    public int a1,a2;
 
     public Figure (int x, int y, int w, int h) {
         this.x = x;
