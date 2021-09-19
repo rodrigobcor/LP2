69cb5b9fa9ce68c92ff202bf3ae1f30503019605
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-09-19 18:42:26 -0300

    Update Arc.java

--- Project/Figures/Arc.java
+++ Project/Figures/Arc.java
@@ -3,21 +3,14 @@ package figures;
 import java.awt.*;
 
 public class Arc extends Figure {
-    private int x, y;
-    private int w, h;
-    private int a1, a2;
-    private Color ccolor;
-    private Color bcolor;
-
+    //private Color ccolor;
+    //private Color bcolor;
     public Arc (int x, int y, int w, int h, int a1, int a2) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
+        super(x,y,w,h);
         this.a1 = a1;
         this.a2 = a2;
-        this.ccolor = ccolor;
-        this.bcolor = bcolor;
+        //this.ccolor = ccolor;
+        //this.bcolor = bcolor;
     }
 
     public void print () {
@@ -27,7 +20,7 @@ public class Arc extends Figure {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(this.ccolor);
+        //g2d.setColor(this.ccolor);
         g2d.drawArc(this.x,this.y, this.w,this.h, this.a1,this.a2);
         //g2d.setColor(this.bcolor);
         //g2d.fillArc(this.x,this.y, this.w,this.h, this.a1,this.a2);
