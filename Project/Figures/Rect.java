fb54a1e211d7763127abae8776c351d34526c640
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-09-26 20:47:42 -0300

    Update Rect.java

--- Project/Figures/Rect.java
+++ Project/Figures/Rect.java
@@ -3,10 +3,10 @@ package figures;
 import java.awt.*;
 
 public class Rect extends Figure {
-    public Rect (int x, int y, int w, int h) {
-        super(x,y,w,h);
+    public Rect (int x, int y, int w, int h, Color ccolor, Color bcolor) {
+        super(x,y,w,h,ccolor,bcolor);
     }
-
+    
     public void print () {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
             this.w, this.h, this.x, this.y);
@@ -14,6 +14,9 @@ public class Rect extends Figure {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(this.ccolor);
         g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.setColor(this.bcolor);
+        g2d.fillRect(this.x,this.y, this.w,this.h);
     }
 }
