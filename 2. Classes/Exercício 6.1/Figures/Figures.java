bbfb7d186dc9dc770e38744868d5a590e7fc1212
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-09-26 15:42:48 -0300

    Create Figures.java

---
+++ 2. Classes/Exercício 6.1/Figures/Figures.java
@@ -0,0 +1,18 @@
+package figures;
+
+import java.awt.Graphics;
+
+public abstract class Figure {
+    public int x, y;
+    public int w, h;
+
+    public Figure (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public abstract void paint (Graphics g);
+    public abstract void print ();
+}
