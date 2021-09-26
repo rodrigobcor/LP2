96187f7d564a00d0948a5326775c47624251d956
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-09-26 20:47:02 -0300

    Update Circle.java

--- Project/Figures/Circle.java
+++ Project/Figures/Circle.java
@@ -3,13 +3,8 @@ package figures;
 import java.awt.*;
 
 public class Circle extends Figure {
-	//private Color ccolor;
-    //private Color bcolor;
-
-    public Circle (int x, int y, int w, int h) {
-        super(x,y,w,h);
-        //this.ccolor = ccolor;
-        //this.bcolor = bcolor;
+    public Circle (int x, int y, int w, int h, Color ccolor, Color bcolor) {
+        super(x,y,w,h,ccolor,bcolor);
     }
 
     public void print () {
@@ -19,9 +14,9 @@ public class Circle extends Figure {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        //g2d.setColor(this.ccolor);
+        g2d.setColor(this.ccolor);
         g2d.drawOval(this.x,this.y, this.w,this.h);
-        //g2d.setColor(this.bcolor);
-        //g2d.fillOval(this.x,this.y, this.w,this.h);
+        g2d.setColor(this.bcolor);
+        g2d.fillOval(this.x,this.y, this.w,this.h);
     }
 }
