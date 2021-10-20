06b74578a8e78ef2291d01ae72d31676aa0fceb3
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-10-20 12:50:31 -0300

    Update Ellipse.java

--- Project/figures/Ellipse.java
+++ Project/figures/Ellipse.java
@@ -4,15 +4,15 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
 public class Ellipse extends Figure {
-    public Ellipse (int x, int y, int w, int h, Color ccolor, Color bcolor) {
-        super(x,y,w,h,ccolor,bcolor);
-    }
+	public Ellipse(int x, int y, int w, int h, Color ccolor, Color bcolor) {
+		super(x, y, w, h, ccolor, bcolor);
+	}
 
-    public void paint (Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(this.ccolor);
-        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-        g2d.setColor(this.bcolor);
-        g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-    }
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(this.ccolor);
+		g2d.draw(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+		g2d.setColor(this.bcolor);
+		g2d.fill(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+	}
 }
