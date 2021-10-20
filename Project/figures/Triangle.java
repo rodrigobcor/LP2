cdc141a217d4b525a698f47b80be3abaf2a026f2
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-10-20 12:50:16 -0300

    Update Triangle.java

--- Project/figures/Triangle.java
+++ Project/figures/Triangle.java
@@ -3,17 +3,17 @@ package figures;
 import java.awt.*;
 
 public class Triangle extends Figure {
-    public Triangle (int x, int y, int w, int h, Color ccolor, Color bcolor) {
-        super(x,y,w,h,ccolor,bcolor);
-    }
+	public Triangle(int x, int y, int w, int h, Color ccolor, Color bcolor) {
+		super(x, y, w, h, ccolor, bcolor);
+	}
 
-    public void paint (Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        int [] tx = {this.x+(this.w/2), this.x+this.w, this.x};
-        int [] ty = {this.y, this.y+this.h, this.y+this.h};
-        g2d.setColor(this.ccolor);
-        g2d.drawPolygon(tx, ty, 3);
-        g2d.setColor(this.bcolor);
-        g2d.fillPolygon(tx, ty, 3);
-    }
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		int[] tx = { this.x + (this.w / 2), this.x + this.w, this.x };
+		int[] ty = { this.y, this.y + this.h, this.y + this.h };
+		g2d.setColor(this.ccolor);
+		g2d.drawPolygon(tx, ty, 3);
+		g2d.setColor(this.bcolor);
+		g2d.fillPolygon(tx, ty, 3);
+	}
 }
