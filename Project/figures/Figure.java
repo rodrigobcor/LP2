ad4e857402c494aca1a671f900d719c4cc36b7f0
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-10-21 17:23:40 -0300

    Update Figure.java

--- Project/figures/Figure.java
+++ Project/figures/Figure.java
@@ -3,20 +3,19 @@ package figures;
 import java.awt.*;
 import ivisible.IVisible;
 
-
 public abstract class Figure implements IVisible {
 	public int x, y;
 	public int w, h;
 	public int a1, a2;
-	Color ccolor;
+//	Color ccolor;
 	Color bcolor;
 
-	public Figure(int x, int y, int w, int h, Color ccolor, Color bcolor) {
+	public Figure(int x, int y, int w, int h, Color bcolor) {
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
-		this.ccolor = ccolor;
+//		this.ccolor = ccolor;
 		this.bcolor = bcolor;
 	}
 
