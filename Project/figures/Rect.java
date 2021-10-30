806624d0ffeba86d3a40c36f9b82adaada47ef87
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-10-30 20:26:28 -0300

    Update Rect.java

--- Project/figures/Rect.java
+++ Project/figures/Rect.java
@@ -3,19 +3,24 @@ package figures;
 import java.awt.*;
 
 public class Rect extends Figure {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public Rect(int x, int y, int w, int h, Color bcolor) {
 		super(x, y, w, h, bcolor);
 	}
 
-	public void paint(Graphics g) {
+	public void paint(Graphics g, boolean focused) {
 		Graphics2D g2d = (Graphics2D) g;
+		
+		if (focused) {
+			g2d.setColor(Color.red);
+			g2d.drawRect(this.x - 1, this.y - 1, this.w + 1, this.h + 1);
+		}
+		
 		g2d.setColor(this.bcolor);
 		g2d.fillRect(this.x, this.y, this.w, this.h);
-	}
-
-	public void foco(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.red);
-		g2d.drawRect(this.x - 1, this.y - 1, this.w + 1, this.h + 1);
 	}
 }
