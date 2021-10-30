3f4e9bb8963ea79449bdfcc3156df4e8332894d7
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-10-30 20:26:41 -0300

    Update IVisible.java

--- Project/ivisible/IVisible.java
+++ Project/ivisible/IVisible.java
@@ -5,7 +5,5 @@ import java.awt.Graphics;
 public interface IVisible {
 	public boolean clicked(int x, int y);
 
-	public void paint(Graphics g);
-	
-	public void foco(Graphics g);
+	public void paint(Graphics g, boolean focused);	
 }
