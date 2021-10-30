daf8e7d22b20657ea25a6c5df765a02969849fd2
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-10-30 20:26:25 -0300

    Update Figure.java

--- Project/figures/Figure.java
+++ Project/figures/Figure.java
@@ -5,6 +5,10 @@ import ivisible.IVisible;
 import java.io.Serializable;
 
 public abstract class Figure implements IVisible, Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public int x, y;
 	public int w, h;
 	public int a1, a2;
@@ -25,5 +29,5 @@ public abstract class Figure implements IVisible, Serializable {
 
 	public boolean clicked(int x, int y) {
 		return (this.x <= x && x <= this.x + this.w && this.y <= y && y <= this.y + this.h);
-	} //COLOCAR NAS SUBCLASSES!!!!!!!!!!!!!!!
+	}
 }
