46832be230e7b7fe86f92baa0c10bd29b68b1177
rodrigodizis <87913655+rodrigodizis@users.noreply.github.com>
2021-11-05 22:18:35 -0300

    Update Button.java

--- Project/Button.java
+++ Project/Button.java
@@ -3,7 +3,7 @@ import figures.Figure;
 import java.awt.*;
 
 public class Button implements IVisible {
-    static int SPC = 20;
+    static int SPC = 40;
     static int DIM = 25;
     static int PAD = 4;
 
@@ -14,7 +14,7 @@ public class Button implements IVisible {
         this.idx = idx;
         this.fig = fig;
         this.fig.x = PAD+SPC;
-        this.fig.y = PAD+SPC + idx*DIM+20;
+        this.fig.y = PAD+SPC + idx*DIM;
         this.fig.w = DIM-PAD*2;
         this.fig.h = DIM-PAD*2;
     }
@@ -27,10 +27,10 @@ public class Button implements IVisible {
         Graphics2D g2d = (Graphics2D) g;
 
         g2d.setColor(focused ? Color.GRAY : Color.LIGHT_GRAY);
-        g2d.fillRect(SPC, SPC+this.idx*DIM+20, DIM, DIM);
+        g2d.fillRect(SPC, SPC+this.idx*DIM, DIM, DIM);
 
         g2d.setColor(Color.BLACK);
-        g2d.drawRect(SPC, SPC+this.idx*DIM+20, DIM, DIM);
+        g2d.drawRect(SPC, SPC+this.idx*DIM, DIM, DIM);
 
         this.fig.paint(g, false);
     }
